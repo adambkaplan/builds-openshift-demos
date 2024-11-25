be551a7c6db8893b113a0235556c99acc23a5d8e
Adam Kaplan <adam.kaplan@redhat.com>
2024-11-25 14:08:09 -0500

    Demo: Build with RHEL Entitlements
    This contains the manifests and code needed to build a container
    that installs RHEL subscription content on OpenShift. It takes
    advantage of the Shared Resource CSI Driver and recent improvements
    to consuming entitlements in RHEL 9.3+.
    
    Notable is the additional RBAC that needs to be granted to the Shared
    Resource CSI driver for each shared object. This is an area for future
    improvement.
    
    Signed-off-by: Adam Kaplan <adam.kaplan@redhat.com>

---
+++ demos/6-entitled-builds/demo.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# shellcheck source-path=lib
+. ../../lib/demo-magic.sh
+
+oc new-project buildah-entitlement
+oc create imagestream buildah-rhel
+clear
+
+p "Suppose our build needs to install RHEL packages:"
+pe "cat build/Containerfile"
+p "With Builds 1.2, we can use the cluster's subscription to install RHEL content!"
+clear
+
+p "To access the subscription, a cluster admin first needs to share it using a SharedSecret object."
+cat 00-rhel-entitlement-sharedsecret.yaml
+pe "oc apply -f 00-rhel-entitlement-sharedsecret.yaml"
+wait
+p "The cluster admin should also create a ClusterRoleBinding to let others use the SharedSecret."
+cat 01-role-use-shared-secret.yaml
+pe "oc apply -f 01-role-use-shared-secret.yaml"
+wait
+clear
+
+p "Next, the cluster admin needs to grant the Shared Resource CSI Driver permission to access the underlying Secret object."
+cat 02-rhel-entitlement-csi-role.yaml
+echo "---"
+cat 02-rhel-entitlement-csi-rolebinding.yaml
+pe "oc apply -f 02-rhel-entitlement-csi-role.yaml"
+pe "oc apply -f 02-rhel-entitlement-csi-rolebinding.yaml"
+wait
+clear
+
+p "After the CSI driver is granted the right RBAC, the service accounts that run the build need permission to use the SharedSecret."
+p "This can be done by anyone with 'admin' permissions in the namespace."
+cat 03-rolebinding-use-shared-secret.yaml
+pe "oc apply -f 03-rolebinding-use-shared-secret.yaml"
+wait
+clear
+
+p "Finally, we configure a build to mount the SharedSecret into the build, and run it!"
+cat 04-entitled-build.yaml
+pe "oc apply -f 04-entitled-build.yaml"
+pe "shp build run buildah-rhel -F"
+wait
+p "Our build ran and successfully installed the RHEL packages!"
+wait
+clear
+
+oc delete project buildah-entitlement
+oc delete rolebinding share-etc-pki-entitlement -n openshift-config-managed
+oc delete role share-etc-pki-entitlement -n openshift-config-managed
+oc delete clusterrole use-share-etc-pki-entitlement
+oc delete sharedsecret etc-pki-entitlement
